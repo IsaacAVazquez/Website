f218a29700f88ef14e7a8de868e1edad91cd5bde
Isaac Vazquez <Isaac@civiteche.com>
2025-06-19 14:05:37 -0400

    Alright

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
+<<<<<<< Updated upstream
   plugins: [
     require('@tailwindcss/postcss'),
     require('autoprefixer'),
   ],
 };
+=======
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
+>>>>>>> Stashed changes
