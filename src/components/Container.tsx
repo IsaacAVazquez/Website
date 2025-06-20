99dcf6c6e90a57c7296606ab207d0328bf7e1b81
Isaac Vazquez <Isaac@civiteche.com>
2025-06-19 22:11:27 -0400

    Build fix

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
+// BEFORE
+// export function Container({ children }: { children: React.ReactNode }) {
+
+// AFTER
+export function Container({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <main className={`max-w-4xl w-full mx-auto py-20 px-4 md:px-10`}>
+    <main className={`max-w-4xl mx-auto w-full px-4 sm:px-8 ${className}`}>
       {children}
     </main>
   );
-};
+}
