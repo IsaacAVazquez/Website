747959ba151a6191332267808b6f4289f8b86b0e
Isaac Vazquez <Isaac@civiteche.com>
2025-06-20 01:25:31 -0400

    sdasdasd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth-dark">
       <head>
         <script
           dangerouslySetInnerHTML={{
